d40c0e34f1cf093952d5c43eb8102c03d160a47f
glevergeois <guillaume.levergeois@gmail.com>
2023-04-18 09:20:44 +0200

    mise sous xampp

--- php/index.php
+++ php/index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-32">
     <meta name="viewport" content="width=device-width">
     <title>Portfolio_Guillaume_Levergeois</title>
-    <link rel="stylesheet" href="/css/index.css">
+    <link rel="stylesheet" href="../css/index.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
@@ -19,20 +19,20 @@
 <main>
     <header>
         <div >
-            <a class="header_item" href="/php/index.html">Home</a>
-            <a class="header_item" href="/php/newsletter.html">Newsletter</a>
-            <a class="header_item" href="contact.html">Contact</a>
+            <a class="header_item" href="index.php">Home</a>
+            <a class="header_item" href="newsletter.php">Newsletter</a>
+            <a class="header_item" href="contact.php">Contact</a>
         </div>    
     </header>
     <ul class="item">
         <li class="item_content">
-            <a href="me.html"></a><h2>Guillaume Levergeois</h2></a>
+            <a href="me.php"></a><h2>Guillaume Levergeois</h2></a>
         </li>
         <li class="item_content">
-            <a href="me.html"></a><h2>Photography</h2></a>
+            <a href="me.php"></a><h2>Photography</h2></a>
         </li>
         <li class="item_content">
-            <a href="me.html"></a><h2>Web-development</h2></a>
+            <a href="me.php"></a><h2>Web-development</h2></a>
         </li>
     </ul>
 </main>
