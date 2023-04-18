c126256614c3fc98a6f1ef6d9f302cc0ce196b3d
glevergeois <guillaume.levergeois@gmail.com>
2023-04-18 12:09:03 +0200

    création animation index

--- php/index.php
+++ php/index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-32">
     <meta name="viewport" content="width=device-width">
     <title>Portfolio_Guillaume_Levergeois</title>
-    <link rel="stylesheet" href="../css/index.css">
+    <link rel="stylesheet" href="../css/index.scss">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
@@ -24,15 +24,24 @@
             <a class="header_item" href="contact.php">Contact</a>
         </div>    
     </header>
-    <ul class="item">
-        <li class="item_content">
+    <ul>
+        <li class="item">
+            <div class="item_background" style="background-image: url('../images/4.JPG')"></div>
+            <div class="item_content">
             <a href="me.php"></a><h2>Guillaume Levergeois</h2></a>
+            </div>
         </li>
-        <li class="item_content">
+        <li class="item">
+            <div class="item_background" style="background-image: url('../images/5.JPG')"></div>
+            <div class="item_content">
             <a href="me.php"></a><h2>Photography</h2></a>
+            </div>
         </li>
-        <li class="item_content">
+        <li class="item">
+            <div class="item_background" style="background-image: url('../images/6.JPG')"></div>
+            <div class="item_content">
             <a href="me.php"></a><h2>Web-development</h2></a>
+            </div>
         </li>
     </ul>
 </main>
