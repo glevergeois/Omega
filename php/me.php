df18adbf97f804b63bc134ab43c399e901f455a5
glevergeois <guillaume.levergeois@gmail.com>
2023-04-19 18:17:03 +0200

    creation page me.php

---
+++ php/me.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en-us">
+
+<head>
+    <meta charset="UTF-32">
+    <meta name="viewport" content="width=device-width">
+    <title>Portfolio_Guillaume_Levergeois</title>
+    <link rel="stylesheet" href="../css/me.scss">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+</head>
+
+
+<body style="font-family: Inter;">
+
+
+
+<main>
+    <?php require_once 'header.php' ?>
+    <section class="item">
+        <article class="about">
+            <h2>About</h2>
+            
+        </article>
+        <article class="bio">
+            <h3>Hey, my name’s Guillaume Levergeois,</h3><br>
+            <p>
+            I’m currently living in Toulouse, in France,<br>
+            studying computer science.<br>
+            I’m passionate about new technologies,
+            photography and nature.<br>
+            Later, I’d love to work as a digital nomad all around<br>
+            the world.
+            </p>
+        </article>
+        <article class="me">
+            <h2>Me ...</h2>
+        </article>
+    </section>
